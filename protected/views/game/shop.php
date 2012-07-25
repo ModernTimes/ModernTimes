bcecba8495740eef52aa6af6bb913c48b444e234
ModernTimes <moderntimesgit@yahoo.com>
2012-07-25 18:27:18 +0200

    Enh: Some beautifications in the shop.php view file
    Delete shops folder in view file folder
    Delete shops/pablo.php view file

--- protected/views/game/shop.php
+++ protected/views/game/shop.php
@@ -3,18 +3,34 @@
  */
 ?>
 
-<h1 align="center"><?php echo $Shop->name; ?></h1>
+<div class="hero-unit" style="padding: 0px; margin: 0px; background: none">
 
-<p><?php echo $Shop->call('getDesc'); ?></p>
+    <h1 align="center" style="margin-bottom: 0.4em"><?php echo $Shop->name; ?></h1>
 
-<?php foreach($Shop->shopItems as $ShopItem) {
-    $this->widget("ItemWidget", array(
-        "item" => $ShopItem->item
-    ));
+    <p style="margin-bottom: 1.6em"><?php echo $Shop->call('getDesc'); ?></p>
+
+    <center><table border="0" cellpadding="10">
+    <?php foreach($Shop->shopItems as $ShopItem) {
+        echo "<tr><td>";
+            $this->widget("ItemWidget", array(
+                "item" => $ShopItem->item
+            ));
+        echo "</td><td style='font-size: 1.4em; width: 6em'>" . 
+                $ShopItem->item->name .
+              "</td><td>";
+        if($ShopItem->cash > 0) {
+            echo $ShopItem->cash . " <img src='" . Yii::app()->getBaseUrl() . "/images/cash.png' width='24' height='24' style='vertical-align: middle' title='Cash'><BR />";
+        }
+        if($ShopItem->favours > 0) {
+            echo $ShopItem->favours . "<img src='" . Yii::app()->getBaseUrl() . "/images/favours.png' width='24' height='24' style='vertical-align: middle' title='Favours'><BR />";
+        }
+        if($ShopItem->kudos > 0) {
+            echo $ShopItem->kudos . " <img src='" . Yii::app()->getBaseUrl() . "/images/kudos.png' width='24' height='24' style='vertical-align: middle' title='Kudos'>";
+        }
+        echo "</td><td>" .
+                "<a class='btn' href='./buyItem?shopID=" . $Shop->id . "&itemID=" . $ShopItem->itemID . "'>Buy</a>" .
+             "</td></tr>";
+    } ?>
+    </table></center>
     
-    echo $ShopItem->item->name;
-    
-    echo $ShopItem->cash . " - " . $ShopItem->favours . " - " . $ShopItem->kudos;
-    echo "<a href='./buyItem?shopID=" . $Shop->id . "&itemID=" . $ShopItem->itemID . "'>buy</a>";
-    echo "<BR />";
-} ?>
+</div>
