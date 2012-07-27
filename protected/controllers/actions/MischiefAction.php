4eb92d3dd46ad2be29052d34811ef7b525121839
ModernTimes <moderntimesgit@yahoo.com>
2012-07-27 09:05:39 +0200

    Feature: MischiefAction now checks for encounters in the encounter stack for a character and resolves those first.
    We can now add encounters to the encounter stack (CharacterEncounters), which is useful for Quests.
    CharacterEncounters.delay is reduced by 1 by Tools->spendTurn

--- protected/controllers/actions/MischiefAction.php
+++ protected/controllers/actions/MischiefAction.php
@@ -1,9 +1,13 @@
 <?php
 /**
  * Resolves doing mischief in a given area, i.e. either redirects to resolving 
- * an encounter or to start a battle against a monster
+ * an encounter or to start a battle against a monster.
+ * 
+ * If there are Encounters in the encounter queue, those are always handled
+ * first.
  * 
  * @uses Area
+ * @uses CharacterEncounters
  * @uses BattleMonsterAction
  * @uses EncounterAction
  * @package Actions
@@ -12,12 +16,25 @@
 class MischiefAction extends CAction {
 
     /**
-     * Lets the Area record generate an episode (monster or encounter), then
-     * starts either EncounterAction or BattleMonsterAction
+     * See above
      * @param string $areaID ID of the area in which the Character wants to do
      * mischief. int, but represented as a string because of $GET
      */
     public function run($areaID) {
+        $Character = CD();
+
+        // Check for Encounters in the encounter queue first
+        foreach($Character->characterEncounters as $CharacterEncounter) {
+            if($CharacterEncounter->delay == 0) {
+                $encounterAction = new EncounterAction($this->controller, "encounter");
+                $encounterAction->encounter = $CharacterEncounter->encounter;
+                $encounterAction->callFromWithinApplication = true;
+                $this->controller->runAction($encounterAction);
+                $CharacterEncounter->delete();
+                return;
+            }
+        }
+        
         // Syntax check: positive integer?
         $validSyntax = (!empty($areaID)
                         // are all characters digits? rules out decimal numbers
@@ -40,7 +57,6 @@ class MischiefAction extends CAction {
         }
 
         // Does the Character meet the requirements for doing mischief here?
-        $Character = CD();
         if(!$area->call("meetsRequirements", $Character)) {
             $this->controller->redirect('index');
         }
