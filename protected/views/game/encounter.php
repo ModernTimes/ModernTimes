4148badf56b103010bb78d2013320e15e965dd80
ModernTimes <moderntimesgit@yahoo.com>
2012-07-27 09:05:52 +0200

    Enh: Beautify encounter view file a bit

--- protected/views/game/encounter.php
+++ protected/views/game/encounter.php
@@ -1,9 +1,7 @@
 <div class="hero-unit" style="padding: 0px; margin: 0px; background: none">
-    <h1><?php echo $encounter->name; ?></h1>
+    <h1 align="center" style="margin-bottom: 0.4em"><?php echo $encounter->name; ?></h1>
     
-    <div>
-        <?php echo $encounter->msg; ?>
-    </div>
+    <p><?php echo $encounter->msg; ?></p>
     
     <?php foreach($encounter->encounterEncounters as $choice) {
         echo $choice->choiceName . "<BR />";
