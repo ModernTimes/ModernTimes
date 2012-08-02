4de35de308a6ae6f329a0e688061235d1d354a0a
ModernTimes <moderntimesgit@yahoo.com>
2012-08-02 16:46:40 +0200

    Enh: don't show items that the character has 0 of

--- protected/components/widgets/views/itemGroup.php
+++ protected/components/widgets/views/itemGroup.php
@@ -5,7 +5,9 @@
  */
 
 foreach($this->CharacterItems as $CharacterItem) {
-    if($CharacterItem->item->type == $this->itemType) {
+    if($CharacterItem->item->type == $this->itemType
+            && $CharacterItem->n > 0) {
+        
         $this->widget('ItemWidget', array(
             "context" => "inventory",
             "item" => $CharacterItem->item,
