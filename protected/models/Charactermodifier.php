38f697a15cb69947ab1a0a4daa7a5ad60fe8e13a
ModernTimes <moderntimesgit@yahoo.com>
2012-08-04 13:08:37 +0200

    Feature: Add lots of new event handlers to hook into the Character's calculations of bonusDamage, resistances, etc. in Charactermodifier

--- protected/models/Charactermodifier.php
+++ protected/models/Charactermodifier.php
@@ -7,10 +7,11 @@ Yii::import('application.models._base.BaseCharactermodifier');
  * Adds a couple of standard event listeners to stat calculation events in 
  * the Character model.
  *
- * implemented so far:
- * - dropCash/Favours/Kudos Abs/Perc
- * - dropItem Perc
- * @todo gainXp/Resoluteness/Willpower/Cunning Abs/Perc
+ * Implemented so far:
+ * - calcAttributes
+ * - calc combat stats (critChance, resistances, bonus damage)
+ * - dropCash/Favours/Kudos
+ * - dropItem chance
  *
  * If you add more event handlers, don't forget to add a line to
  * detachFromCharacter()
@@ -18,6 +19,8 @@ Yii::import('application.models._base.BaseCharactermodifier');
  * Can be "overridden" by specialness behavior classes for items, skills, etc.
  * 
  * See BaseCharactermodifier for a list of attributes and related Models
+ * 
+ * @todo gainXp/Resoluteness/Willpower/Cunning Abs/Perc
  * 
  * @uses CharacterModifierBehavior
  * @uses SpecialnessBehavior
@@ -61,6 +64,63 @@ class Charactermodifier extends BaseCharactermodifier {
         if($this->dropItemPerc != 0) {
             $Character->onCalcDropItemBonus = array($this, 'reactToOnCalcDropItemBonus');
         }
+        
+        if($this->critChancePerc != 0) {
+            $Character->onCalcCritChance = array($this, 'reactToOnCalcCritChance');
+        }
+        if($this->damageNormalAbs != 0 || $this->damageNormalPerc != 0) {
+            $Character->onCalcBonusDamageNormal = array($this, 'reactToOnCalcBonusDamageNormal');
+        }
+        if($this->damageEnvyAbs != 0 || $this->damageEnvyPerc != 0) {
+            $Character->onCalcBonusDamageEnvy = array($this, 'reactToOnCalcBonusDamageEnvy');
+        }
+        if($this->damageGreedAbs != 0 || $this->damageGreedPerc != 0) {
+            $Character->onCalcBonusDamageGreed = array($this, 'reactToOnCalcBonusDamageGreed');
+        }
+        if($this->damageGluttonyAbs != 0 || $this->damageGluttonyPerc != 0) {
+            $Character->onCalcBonusDamageGluttony = array($this, 'reactToOnCalcBonusDamageGluttony');
+        }
+        if($this->damageLustAbs != 0 || $this->damageLustPerc != 0) {
+            $Character->onCalcBonusDamageLust = array($this, 'reactToOnCalcBonusDamageLust');
+        }
+        if($this->damagePrideAbs != 0 || $this->damagePridePerc != 0) {
+            $Character->onCalcBonusDamagePride = array($this, 'reactToOnCalcBonusDamagePride');
+        }
+        if($this->damageSlothAbs != 0 || $this->damageSlothPerc != 0) {
+            $Character->onCalcBonusDamageSloth = array($this, 'reactToOnCalcBonusDamageSloth');
+        }
+        if($this->damageWrathAbs != 0 || $this->damageWrathPerc != 0) {
+            $Character->onCalcBonusDamageWrath = array($this, 'reactToOnCalcBonusDamageWrath');
+        }
+        
+        if($this->resistanceAbs != 0) {
+            $Character->onCalcResistanceAbs = array($this, 'reactToOnCalcResistanceAbs');
+        }
+        if($this->resistanceLevelNormal != 0) {
+            $Character->onCalcResistanceLevelNormal = array($this, 'reactToOnCalcResistanceLevelNormal');
+        }
+        if($this->resistanceLevelEnvy != 0) {
+            $Character->onCalcResistanceLevelEnvy = array($this, 'reactToOnCalcResistanceLevelEnvy');
+        }
+        if($this->resistanceLevelGreed != 0) {
+            $Character->onCalcResistanceLevelGreed = array($this, 'reactToOnCalcResistanceLevelGreed');
+        }
+        if($this->resistanceLevelGluttony != 0) {
+            $Character->onCalcResistanceLevelGluttony = array($this, 'reactToOnCalcResistanceLevelGluttony');
+        }
+        if($this->resistanceLevelLust != 0) {
+            $Character->onCalcResistanceLevelLust = array($this, 'reactToOnCalcResistanceLevelLust');
+        }
+        if($this->resistanceLevelPride != 0) {
+            $Character->onCalcResistanceLevelPride = array($this, 'reactToOnCalcResistanceLevelPride');
+        }
+        if($this->resistanceLevelSloth != 0) {
+            $Character->onCalcResistanceLevelSloth = array($this, 'reactToOnCalcResistanceLevelSloth');
+        }
+        if($this->resistanceLevelWrath != 0) {
+            $Character->onCalcResistanceLevelWrath = array($this, 'reactToOnCalcResistanceLevelWrath');
+        }
+        
     }
 
     /**
@@ -81,6 +141,28 @@ class Charactermodifier extends BaseCharactermodifier {
         $Character->detachEventHandler("onGainKudos", array($this, 'reactToonGainKudos'));
         
         $Character->detachEventHandler("onCalcDropItemBonus", array($this, 'reactToOnCalcDropItemBonus'));
+        
+        $Character->detachEventHandler("onCalcResistanceAbs", array($this, 'reactToOnCalcResistanceAbs'));
+        $Character->detachEventHandler("onCalcResistanceLevelNormal", array($this, 'reactToOnCalcResistanceLevelNormal'));
+        $Character->detachEventHandler("onCalcResistanceLevelEnvy", array($this, 'reactToOnCalcResistanceLevelEnvy'));
+        $Character->detachEventHandler("onCalcResistanceLevelGreed", array($this, 'reactToOnCalcResistanceLevelGreed'));
+        $Character->detachEventHandler("onCalcResistanceLevelGluttony", array($this, 'reactToOnCalcResistanceLevelGluttony'));
+        $Character->detachEventHandler("onCalcResistanceLevelLust", array($this, 'reactToOnCalcResistanceLevelLust'));
+        $Character->detachEventHandler("onCalcResistanceLevelPride", array($this, 'reactToOnCalcResistanceLevelPride'));
+        $Character->detachEventHandler("onCalcResistanceLevelSloth", array($this, 'reactToOnCalcResistanceLevelSloth'));
+        $Character->detachEventHandler("onCalcResistanceLevelWrath", array($this, 'reactToOnCalcResistanceLevelWrath'));
+        
+        $Character->detachEventHandler("onCalcCritChance", array($this, 'reactToOnCalcCritChance'));
+        
+        $Character->detachEventHandler("onCalcBonusDamageNormal", array($this, 'reactToOnCalcBonusDamageNormal'));
+        $Character->detachEventHandler("onCalcBonusDamageEnvy", array($this, 'reactToOnCalcBonusDamageEnvy'));
+        $Character->detachEventHandler("onCalcBonusDamageGreed", array($this, 'reactToOnCalcBonusDamageGreed'));
+        $Character->detachEventHandler("onCalcBonusDamageGluttony", array($this, 'reactToOnCalcBonusDamageGluttony'));
+        $Character->detachEventHandler("onCalcBonusDamageLust", array($this, 'reactToOnCalcBonusDamageLust'));
+        $Character->detachEventHandler("onCalcBonusDamagePride", array($this, 'reactToOnCalcBonusDamagePride'));
+        $Character->detachEventHandler("onCalcBonusDamageSloth", array($this, 'reactToOnCalcBonusDamageSloth'));
+        $Character->detachEventHandler("onCalcBonusDamageWrath", array($this, 'reactToOnCalcBonusDamageWrath'));
+
     }    
     
     /**
@@ -121,13 +203,169 @@ class Charactermodifier extends BaseCharactermodifier {
     }
     /**
      * Basic event handler
-     * Adds bonusAbs und bonusPerc according to the Model record's attributes
+     * Adds bonusAbs und bonusPerc
      * @param CEvent $event with BonusCollectorBehavior
      */
     public function reactToOnCalcCunning($event) { 
         $event->increaseBonusAbs($this->cunning);
         $event->increaseBonusPerc($this->cunningPerc);
     }    
+    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceAbs($event) { 
+        $event->increaseBonusAbs($this->resistanceAbs);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelNormal($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelNormal);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelEnvy($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelEnvy);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelGreed($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelGreed);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelGluttony($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelGluttony);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelLust($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelLust);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelPride($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelPride);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelSloth($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelSloth);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcResistanceLevelWrath($event) { 
+        $event->increaseBonusAbs($this->resistanceLevelWrath);
+    }    
+    
+    /**
+     * Basic event handler
+     * Adds bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcCritChance($event) { 
+        $event->increaseBonusPerc($this->critChancePerc);
+    }    
+
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageNormal($event) { 
+        $event->increaseBonusAbs($this->damageNormalAbs);
+        $event->increaseBonusPerc($this->damageNormalPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageEnvy($event) { 
+        $event->increaseBonusAbs($this->damageEnvyAbs);
+        $event->increaseBonusPerc($this->damageEnvyPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageGreed($event) { 
+        $event->increaseBonusAbs($this->damageGreedAbs);
+        $event->increaseBonusPerc($this->damageGreedPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageGluttony($event) { 
+        $event->increaseBonusAbs($this->damageGluttonyAbs);
+        $event->increaseBonusPerc($this->damageGluttonyPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageLust($event) { 
+        $event->increaseBonusAbs($this->damageLustAbs);
+        $event->increaseBonusPerc($this->damageLustPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamagePride($event) { 
+        $event->increaseBonusAbs($this->damagePrideAbs);
+        $event->increaseBonusPerc($this->damagePridePerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageSloth($event) { 
+        $event->increaseBonusAbs($this->damageSlothAbs);
+        $event->increaseBonusPerc($this->damageSlothPerc);
+    }    
+    /**
+     * Basic event handler
+     * Adds bonusAbs und bonusPerc
+     * @param CEvent $event with BonusCollectorBehavior
+     */
+    public function reactToOnCalcBonusDamageWrath($event) { 
+        $event->increaseBonusAbs($this->damageWrathAbs);
+        $event->increaseBonusPerc($this->damageWrathPerc);
+    }    
+    
 
     /**
      * Basic event handler
@@ -137,7 +375,7 @@ class Charactermodifier extends BaseCharactermodifier {
      * @param array $acceptedSources list of scenarios in which the event observer
      * should do its work
      */
-    public function reactToonGainCash($event, $acceptedSources = array('battle')) {
+    public function reactToOnGainCash($event, $acceptedSources = array('battle')) {
         if(in_array($event->params['from'], $acceptedSources)) {
             $event->increaseBonusAbs($this->dropCash);
             $event->increaseBonusPerc($this->dropCashPerc);
@@ -151,7 +389,7 @@ class Charactermodifier extends BaseCharactermodifier {
      * @param array $acceptedSources list of scenarios in which the event observer
      * should do its work
      */
-    public function reactToonGainFavours($event, $acceptedSources = array('battle')) {
+    public function reactToOnGainFavours($event, $acceptedSources = array('battle')) {
         if(in_array($event->params['from'], $acceptedSources)) {
             $event->increaseBonusAbs($this->dropFavours);
             $event->increaseBonusPerc($this->dropFavoursPerc);
@@ -165,7 +403,7 @@ class Charactermodifier extends BaseCharactermodifier {
      * @param array $acceptedSources list of scenarios in which the event observer
      * should do its work
      */
-    public function reactToonGainKudos($event, $acceptedSources = array('battle')) {
+    public function reactToOnGainKudos($event, $acceptedSources = array('battle')) {
         if(in_array($event->params['from'], $acceptedSources)) {
             $event->increaseBonusAbs($this->dropKudos);
             $event->increaseBonusPerc($this->dropKudosPerc);
