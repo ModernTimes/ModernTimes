2d3f2441f056be39fbb581025aa86b85d9cb1dee
ModernTimes <moderntimesgit@yahoo.com>
2012-08-05 21:10:46 +0200

    Enh: Show only the tutorial quest display in home as long as it's not completed

--- protected/views/game/home/home.php
+++ protected/views/game/home/home.php
@@ -1,33 +1,29 @@
+<div class="hero-unit" style="padding: 0px; margin: 0px; background: none">
+    
 <h1>Home, sweet home</h1><BR />
 
 <?php 
-    if(!empty($tutorialQuestDisplay)) {
-        echo $tutorialQuestDisplay;
-    }
-?>
-
-<h2>Personal todos</h2>
-
-<?php
-$wokenUp = false;
-if(count($currentCharacterQuests) > 0) {
-    foreach($currentCharacterQuests as $CharacterQuest) {
-        if($CharacterQuest->questID == 1 && 
-                $CharacterQuest->state == "completed") {
-            $wokenUp = true;
-        }
-        $this->widget('QuestWidget', array(
-            "CharacterQuest" => $CharacterQuest
-        )); 
-    }
+if(!empty($tutorialQuestDisplay)) {
+    echo $tutorialQuestDisplay;
 } else {
-    echo "<p>You don't have any personal projects right now.</p>";
-}
 
-if($wokenUp) {
+    echo "<h2>Personal todos</h2>";
+
+    if(count($currentCharacterQuests) > 0) {
+        foreach($currentCharacterQuests as $CharacterQuest) {
+            $this->widget('QuestWidget', array(
+                "CharacterQuest" => $CharacterQuest
+            )); 
+        }
+    } else {
+        echo "<p>You don't have any personal projects right now.</p>";
+    }
+
     echo "<div class='btn-group'>
             <button class='btn btn-large'><i class='icon-time'></i>&nbsp;</button>
             <button class='btn btn-large btn-success'>Rest</button>
         </div>";
 }
 ?>
+
+</div>
